8ba4bfd5871e76d92f54c176594fef0f666b0e26
PepperMcLean <jackmcl206@gmail.com>
2021-11-05 20:25:36 -0500

    commit tree 0d4157cc4091a5720522bacde266799daf9a8058

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def add
+    cart << product_params
+    redirect_to :products
+  end
+
+  private
+
+  def product_params
+    params.require(:product)
+  end
+
+end
